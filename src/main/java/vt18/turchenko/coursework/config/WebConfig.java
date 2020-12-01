57b5b468ac5a81244885daee57c43fca4a9d33ec
Vadik <turchenkovadim2000@gmail.com>
2020-12-01 21:12:38 +0200

    Add base configuration for dispatcher servlet bootstrap

---
+++ src/main/java/vt18/turchenko/coursework/config/WebConfig.java
@@ -0,0 +1,22 @@
+package vt18.turchenko.coursework.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("vt18.turchenko.coursework")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("/public/");
+        if (!registry.hasMappingForPattern("/webjars/**")) {
+            registry.addResourceHandler("/webjars/**")
+                    .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        }
+    }
+}
